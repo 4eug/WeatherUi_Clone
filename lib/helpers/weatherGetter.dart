d9bcc9cbcd9263444f46064f00db492728a9ab9a
Master Sofua Desmond <mrsofua@gmail.com>
2021-08-21 15:51:02 +0000

    Create weatherGetter.dart

---
+++ lib/helpers/weatherGetter.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:weather_app_clone/helpers/api.dart';
+
+import 'package:weather_app_clone/model/weather.dart';
+
+final String callbackURL =
+    'https://api.weatherapi.com/v1/forecast.json?key=$apiKey&q=Accra&days=3&aqi=no&alerts=no';
+
+class WeatherService {
+  Future<WeatherForecast> fetchWeather() async {
+    http.Response response = await http.get(Uri.parse(callbackURL));
+    if (response.statusCode == 200) {
+      WeatherForecast weather =
+          WeatherForecast.fromJson(jsonDecode(response.body));
+      print(weather.forecastDay[0].date);
+      return weather;
+      //returning parsed data from json issue
+
+    } else {
+      throw Exception('something went wrong, ${response.statusCode}');
+    }
+  }
+}
